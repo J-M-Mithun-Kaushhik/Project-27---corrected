e38cb48a138e7783ccf31a3c5125dad277027f7d
J-M-Mithun-Kaushhik <73286357+J-M-Mithun-Kaushhik@users.noreply.github.com>
2020-12-27 15:31:52 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,21 @@
+class Bob {
+    constructor(x, y, radius) {
+      var options ={
+        isStatic: false,
+        restitution: 1,
+        friction: 0.1,
+        density: 0.5
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      World.add(world, this.body);
+
+      
+    }
+    display(){
+      var pos =this.body.position;
+      fill("pink");
+      circle(pos.x,pos.y,this.radius*2);
+    }
+  }
+  
